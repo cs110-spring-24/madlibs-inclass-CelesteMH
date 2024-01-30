3ae9c20a9988957b125372bbebdfc398c68c01e4
CelesteMH <157657933+CelesteMH@users.noreply.github.com>
2024-01-30 00:14:11 +0000

    Updated

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,7 @@
 # TODO Create your inputs, variables, and add your story here!
+
+noun = input("give me a noun: ")
+adjective = input("give me an adjective: ")
+verb = input("give me an active verb: ")
+
+print(noun,"was", adjective, "while", verb)
